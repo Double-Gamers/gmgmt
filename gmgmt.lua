115539d309e35394185fc12e647d32c39067153e
JRSmile <jrsmile@users.noreply.github.com>
2023-12-04 16:15:30 +0100

    1st try

---
+++ gmgmt.lua
@@ -0,0 +1,28 @@
+-- Dependencies: AceAddon-3.0, AceComm-3.0, LibSerialize, LibDeflate
+gmgmt = LibStub("AceAddon-3.0"):NewAddon("G.M.G.M.T", "AceComm-3.0")
+local LibSerialize = LibStub("LibSerialize")
+local LibDeflate = LibStub("LibDeflate")
+
+function gmgmt:OnEnable()
+    self:RegisterComm("G.M.G.M.T")
+end
+
+-- With compression (recommended):
+function gmgmt:Transmit(data)
+    local serialized = LibSerialize:Serialize(data)
+    local compressed = LibDeflate:CompressDeflate(serialized)
+    local encoded = LibDeflate:EncodeForWoWAddonChannel(compressed)
+    self:SendCommMessage("G.M.G.M.T", encoded, "WHISPER", UnitName("player"))
+end
+
+function gmgmt:OnCommReceived(prefix, payload, distribution, sender)
+    local decoded = LibDeflate:DecodeForWoWAddonChannel(payload)
+    if not decoded then return end
+    local decompressed = LibDeflate:DecompressDeflate(decoded)
+    if not decompressed then return end
+    local success, data = LibSerialize:Deserialize(decompressed)
+    if not success then return end
+
+    -- Handle `data`
+    print(data)
+end
