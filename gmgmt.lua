a255a83ce5dc9c64d81a6cf323d1970cfbe25766
JRSmile <jan@behead.de>
2024-03-21 08:17:28 +0100

    update TOC + add Testnotify add weakaura integration

--- gmgmt.lua
+++ gmgmt.lua
@@ -1,16 +1,11 @@
+local AddonName, OptionsPrivate = ...
 -- Dependencies: AceAddon-3.0, AceComm-3.0, AceSerializer-3.0, LibDeflate
-gmgmt = LibStub("AceAddon-3.0"):NewAddon("G.M.G.M.T", "AceComm-3.0","AceConsole-3.0", "AceEvent-3.0", "LibAboutPanel-2.0","AceBucket-3.0")
+gmgmt = LibStub("AceAddon-3.0"):NewAddon("G.M.G.M.T", "AceComm-3.0","AceConsole-3.0", "AceEvent-3.0", "LibAboutPanel-2.0","AceBucket-3.0","AceTimer-3.0")
 local WagoAnalytics = LibStub("WagoAnalytics"):Register("BNBeQxGx")
 local LibSerialize = LibStub("AceSerializer-3.0")
 local LibDeflate = LibStub("LibDeflate")
+local db = LibStub("AceDB-3.0"):New("G.M.G.M.T", defaults, true)
 local day, monthOffset
-
--- declare defaults to be used in the DB
-local defaults = {
-    profile = {
-    setting = true,
-    }
-}
 
 function gmgmt:OnInitialize()
     local options = {
@@ -20,8 +15,7 @@ function gmgmt:OnInitialize()
     }
     
     -- Assuming `options` is your top-level options table and `self.db` is your database:
-    self.db = LibStub("AceDB-3.0"):New("gmgmtDB", defaults, true)
-    options.args.profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+    options.args.profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(db)
 
     -- support for LibAboutPanel-2.0
     options.args.aboutTab = self:AboutOptionsTable("gmgmt")
@@ -46,8 +40,6 @@ function gmgmt:DelayedImport()
             local msg
             for k,v in pairs(list) do
                 if not (v.isChild or v.name == "VERSION") then
-                    --self:Print(v.name)
-                    --self:Print(v.data.uid)
                     if v.name == "G.M.G.M.T" then
                         found = v.data
                     end
@@ -127,11 +119,10 @@ function gmgmt:listAuras()
     return sortedTable
 end
 
+
 function gmgmt:OnEnable()
     self:RegisterComm("G.M.G.M.T")
     self:RegisterBucketEvent("CALENDAR_UPDATE_PENDING_INVITES",1 ,"RespondTo_CalendarUpdatePendingInvites")
-    --self:RegisterEvent("CALENDAR_UPDATE_EVENT_LIST","RespondTo_CalendarUpdatePendingInvites")
-    --if ( not IsAddOnLoaded("Blizzard_Calendar") ) then UIParentLoadAddOn("Blizzard_Calendar") end
     gmgmt:QueryCalendar()
     if IsAddOnLoaded("WeakAuras") then
         self:ScheduleTimer("DelayedImport", 30)
@@ -143,11 +134,11 @@ function gmgmt:OnEnable()
     end
 end
 
-function gmgmt:Transmit(data, channel)
+function gmgmt:Transmit(data, channel, target)
     local serialized = LibSerialize:Serialize(data)
     local compressed = LibDeflate:CompressDeflate(serialized)
     local encoded = LibDeflate:EncodeForWoWAddonChannel(compressed)
-    self:SendCommMessage("G.M.G.M.T", encoded, channel)
+    self:SendCommMessage("G.M.G.M.T", encoded, channel, target)
 end
 
 function gmgmt:OnCommReceived(prefix, payload, distribution, sender)
@@ -159,64 +150,31 @@ function gmgmt:OnCommReceived(prefix, payload, distribution, sender)
     if not success then return end
 
     -- Handle `data`
-    if data.command == "gnotify " or data.command == "rnotify" then
-        data = data.data
-        WagoAnalytics:IncrementCounter("triggered")
-        self:Print(data)
-        local voiceID = C_TTSSettings.GetVoiceOptionID(0)
-        PlaySoundFile("Interface\\Addons\\gmgmt\\Media\\sfx\\announcement-sound-5-21465.mp3", "SFX")
-        C_VoiceChat.SpeakText(voiceID, data, Enum.VoiceTtsDestination.LocalPlayback, 0, 100)
-        if IsAddOnLoaded("WeakAuras") then
-            WeakAuras.ScanEvents("GMGMT_CUSTOM_EVENT", data)
-        end
+    WagoAnalytics:IncrementCounter("triggered")
+    self:Print(data)
+    local voiceID = C_TTSSettings.GetVoiceOptionID(0)
+    PlaySoundFile("Interface\\Addons\\gmgmt\\Media\\sfx\\announcement-sound-5-21465.mp3", "SFX")
+    C_VoiceChat.SpeakText(voiceID, data, Enum.VoiceTtsDestination.LocalPlayback, 0, 100)
+    if IsAddOnLoaded("WeakAuras") then
+        WeakAuras.ScanEvents("GMGMT_CUSTOM_EVENT", data)
     end
 end
 
 function gmgmt:QueryCalendar()
-
-	--self:Print("Querying Calendar")
-
 	C_Calendar.OpenCalendar();
-
-	--self:Print("Generating list of months and weekdays and event types")
 	eventTypes = { C_Calendar.EventGetTypes() }
-
 	local d  = C_DateAndTime.GetCurrentCalendarTime()
-	--self:Print("CalendarGetDate returned: ", d.weekday,"(",CALENDAR_WEEKDAY_NAMES[d.weekday],")", d.monthDay, d.month, d.year)
-	--self:Print("Setting calendar to month/year : ", d.month, d.year)
 	C_Calendar.SetAbsMonth(d.month,d.year)
 	monthOffset = 0
-	--self:Print("Getting Number of events for day/monthoffset : ", d.monthDay,monthOffset)
 	numEvents = C_Calendar.GetNumDayEvents(monthOffset, d.monthDay)
-	--self:Print("Number of events/monthOffset = ", numEvents, monthOffset)
-
 	gmgmt:RespondTo_CalendarUpdatePendingInvites()
 end
 
 function gmgmt:RespondTo_CalendarUpdatePendingInvites()
-
 	local d  = C_DateAndTime.GetCurrentCalendarTime()
     monthOffset = 0
-	--self:Print("Getting Number of events for day/monthoffset : ", d.monthDay,monthOffset)
 	numEvents = C_Calendar.GetNumDayEvents(monthOffset, d.monthDay)
-	--self:Print("Number of events/monthOffset = ", numEvents, monthOffset)
 	if ( numEvents == 0 ) then return end
-	
-	--self:Print("Laufende Ereignisse : ");
-	for index = 1,numEvents do
-		--title, hour, minute, calendarType, sequenceType, eventType, texture, modStatus, inviteStatus, invitedBy, difficulty, inviteType = C_Calendar.GetDayEvent(monthOffset, d.monthDay, index)
-        --currEvent = C_Calendar.GetDayEvent(monthOffset, d.monthDay, index)
-		--self:Print("Title:", currEvent.title, "Start:", currEvent.startTime.hour..":"..currEvent.startTime.minute, "Calendar Type:", currEvent.calendarType, "Sequence Type:",currEvent.sequenceType, "Event Type:", currEvent.eventType, "Invite Status:", currEvent.inviteStatus, "Invited By:", currEvent.invitedBy, "Difficulty:", currEvent.difficulty, "Invite Type:", currEvent.inviteType);
-        --self:Print("+", currEvent.title);
-		--if ( currEvent.calendarType == GUILD_EVENT or currEvent.calendarType == GUILD_ANNOUNCEMENT or currEvent.calendarType == PLAYER ) then
-			--self:Print("This is a guild event or announcement or a player created event")
-			--if ( currEvent.inviteStatus == 1 or currEvent.inviteStatus == 8 or currEvent.inviteStatus == 5 ) then		
-				--self:Print("You have been invited or have not signed up or the invite is out");
-			--end
-		--end
-	end
-
-    --self:Print("Getting Number of Pending Invites")
 	numInvites = C_Calendar.GetNumPendingInvites()
 	if ( numInvites > 0 ) then self:Print("|cFFFF0000Du musst noch auf ", numInvites, " Kalenderinvite(s) antworten!!!|r") end
 end
@@ -224,20 +182,23 @@ end
 gmgmt:RegisterChatCommand("gnotify", "gnotify")
 
 function gmgmt:gnotify(input)
-    local send{}
-    send.command = "gnotify"
-    send.data = input
-    gmgmt:Transmit(send,"GUILD")
+    gmgmt:Transmit(input,"GUILD",nil)
 end
 
 gmgmt:RegisterChatCommand("rnotify", "rnotify")
 
 function gmgmt:rnotify(input)
-    local send{}
-    send.command = "rnotify"
-    send.data = input
-    gmgmt:Transmit(send,"RAID")
+    gmgmt:Transmit(input,"RAID", nil)
 end
+
+gmgmt:RegisterChatCommand("testnotify", "testnotify")
+
+function gmgmt:testnotify(input)
+    gmgmt:Transmit(input,"WHISPER",UnitName("player"))
+end
+
+
+
 
 function gmgmt:RegisterSharedMedia()
     local LSM = LibStub("LibSharedMedia-3.0")
